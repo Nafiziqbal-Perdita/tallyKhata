8f1ca156cd0e0fc3b36834e98ac4d0d856407230
Nafiz Iqbal <nafiziqbal.mo@gmail.com>
2026-02-25 01:41:02 -0800

    set up expo project with nativewind

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-2xl font-bold text-blue-500" >Hello, TallyKhata!</Text>
+    </View>
+  );
+}
