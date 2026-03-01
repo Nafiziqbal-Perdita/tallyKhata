3a5ccaaf9ed125e95e18921adf68b964060393ff
Nafiz Iqbal <nafiziqbal.mo@gmail.com>
2026-03-01 02:00:15 -0800

    added the new set of EXPO55 sdk

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-100">
+      <Text className="rounded-lg bg-blue-600 px-4 py-2 text-lg font-semibold text-white">
+        NativeWind is working
+      </Text>
+    </View>
+  );
+}
+
+
